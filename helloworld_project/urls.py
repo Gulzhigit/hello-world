98cd90d9d99de9597d2629db2ffcc848c7e26c40
Gulzhigit <mr.ksedos@gmail.com>
2020-07-03 19:12:52 +0600

    initial commit

---
+++ helloworld_project/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include # новое добавление
+ 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('pages.urls')), # новое добавление
+]
